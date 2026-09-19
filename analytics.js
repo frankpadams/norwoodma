3308f49afd3a986d4caeb34f6bedeaa8dde786af
frankpadams <frankpadams@gmail.com>
2026-09-19 18:26:37 -0400

    analytics: add Clarity loader

---
+++ analytics.js
@@ -0,0 +1,1 @@
+(function(){var d=document,s=d.createElement('script'),f=d.getElementsByTagName('script')[0];s.async=true;s.src='https://www.clarity.ms/tag/'+'ykyity024p';f.parentNode.insertBefore(s,f);})();
