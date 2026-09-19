7b3ac0d921ee4dabe374f58a5c66cc620fbf3e13
frankpadams <frankpadams@gmail.com>
2026-09-19 18:47:19 -0400

    analytics: enable Clarity and Cloudflare Web Analytics

--- analytics.js
+++ analytics.js
@@ -1,1 +1,5 @@
-(function(){var d=document,s=d.createElement('script'),f=d.getElementsByTagName('script')[0];s.async=true;s.src='https://www.clarity.ms/tag/'+'ykyity024p';f.parentNode.insertBefore(s,f);})();
+(function(){
+  var d=document, first=d.getElementsByTagName('script')[0];
+  var clarity=d.createElement('script'); clarity.async=true; clarity.src='https://www.clarity.ms/tag/'+'ykyity024p'; first.parentNode.insertBefore(clarity,first);
+  var cf=d.createElement('script'); cf.type='module'; cf.src='https://static.cloudflareinsights.com/beacon.min.js'; cf.setAttribute('data-cf-beacon','{"token":"49f7256fc503442b94e2bf83181600c3"}'); first.parentNode.insertBefore(cf,first);
+})();
